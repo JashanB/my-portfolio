c1b173b677158d8aa533d0095f5c8ef6135d703c
JashanB <jashan_brar50@hotmail.com>
2020-05-18 20:27:20 -0400

    building main

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Main from '../main';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
+  //simply react route to either main or blog
+  //have header in app - displays contact info about me
   return (
     <div className="App">
+      <h1>Jashan Brar</h1>
+      <Main/>
     </div>
   );
 }
---
+++ src/components/main/Main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Main = (props) => {
+  //take projects
+  //make list item for each one - pass down info about project
+  //have container which holds all projects - href to project page
+  const projects = {
+    "better-trip": {
+      name: "Bettr Trip.",
+      frontend: "React",
+      backend: "Ruby on Rails",
+      api: "Google Maps JS, Google Directions Services, Google Distance Matrix, Google Places, DarkSky Forecast/TimeMachine",
+      database: "psql",
+      
+    }
+  }
+}
+
+export default Main
