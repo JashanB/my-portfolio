1cae715c1158d4cd131acde7576b891d5b9bbcee
JashanB <jashan_brar50@hotmail.com>
2020-05-18 21:24:37 -0400

    added projects objects

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,19 +1,12 @@
 import React from 'react';
+import './Main.css';
+import { projects } from "./projects";
 
 const Main = (props) => {
   //take projects
   //make list item for each one - pass down info about project
   //have container which holds all projects - href to project page
-  const projects = {
-    "better-trip": {
-      name: "Bettr Trip.",
-      frontend: "React",
-      backend: "Ruby on Rails",
-      api: "Google Maps JS, Google Directions Services, Google Distance Matrix, Google Places, DarkSky Forecast/TimeMachine",
-      database: "psql",
-      
-    }
-  }
+  const makeProjects = projects.map()
 }
 
 export default Main
---
+++ src/components/main/projects.js
@@ -0,0 +1,70 @@
+const projects = {
+  1: {
+    name: "Bettr Trip.",
+    frontend: "React",
+    backend: "Ruby on Rails",
+    api: "Google Maps JS, Google Directions Services, Google Distance Matrix, Google Places, DarkSky Forecast/TimeMachine",
+    database: "psql",
+    image: "",
+    type: "blog",
+    testing: "",
+    description: "A web tool that allows users to plan trips while referencing google map. Weather, travel duration, and nearby interesting points are also shown to allow users make better decisions."
+  },
+  2: {
+    name: "WeatherApp",
+    frontend: "React",
+    backend: "Ruby on Rails",
+    api: "Google Maps JS, DarkSky Forecast/TimeMachine, CanvasJS",
+    database: "psql",
+    image: "",
+    type: "blog",
+    testing: "",
+    description: "Weather Forecast for your favourite locations - 48 hourly, weekly and historical comparisons"
+  },
+  3: {
+    name: "Interview Scheduler",
+    frontend: "React",
+    backend: "Node/Express",
+    api: "",
+    database: "",
+    image: "",
+    type: "blog",
+    testing: "Jest and Cypress",
+    description: "A single page React application that allows users to book and cancel interviews.📆 Tested using both Jest and Cypress"
+  },
+  4: {
+    name: "Newton",
+    frontend: "HTML/CSS",
+    backend: "Node.js/Express RESTful API",
+    api: "",
+    database: "psql",
+    image: "",
+    type: "blog",
+    testing: "",
+    description: "Pinterest-like app for learners. React web app that allows learners to save learning resources like tutorials, blogs and videos in a central place that is publicly available to any user."
+  },
+  5: {
+    name: "Game of Life",
+    frontend: "React/Redux",
+    backend: "",
+    api: "",
+    database: "",
+    image: "",
+    type: "blog",
+    testing: "",
+    description: "Conway's game of life using React and Redux"
+  },
+  6: {
+    name: "Number Guesser",
+    frontend: "React",
+    backend: "",
+    api: "",
+    database: "",
+    image: "",
+    type: "deploy",
+    testing: "Unit and integration tested with React testing library and Jest",
+    description: "Chrome extension new tab override - built to practice testing"
+  },
+};
+
+export { projects };
